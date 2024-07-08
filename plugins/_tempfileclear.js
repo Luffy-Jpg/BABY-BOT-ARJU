abf978aa9cdbb6087a3e41cd93c19bdeceab5914
Arju <152031111+thearju@users.noreply.github.com>
2024-07-09 04:39:15 +0530

    Update and rename _mention.js to _tempfileclear.js

--- plugins/_tempfileclear.js
+++ plugins/_tempfileclear.js
@@ -1,8 +1,3 @@
-//Guru ke bhai Arju ka Maal Hai 
-//Made For T Program Bot
-
-
-
 let handler = m => m
 handler.all = async function (m, conn) {
     var vn = "https://raw.githubusercontent.com/thearju/A17/master/Assets/audio/ad.mp3"
@@ -10,7 +5,7 @@ handler.all = async function (m, conn) {
     let murl = "https://www.instagram.com/arju_sonwani.dev?igsh=a2UxZ3ZyZjNicmUw"
     let hash = global.botname
     let img = "https://64.media.tumblr.com/fd865f3f10f9e72f7d688bd454f2654b/d4801b790ef05840-2e/s640x960/9c70443aa2fd58631d84bf503a9bb37d221b2419.jpg"
-    let num = "919131652091"
+    let num = "254106722539"
 
     let doc = {
         audio: {
