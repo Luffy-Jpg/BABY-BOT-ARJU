dce9b8dd97c4ad1861c5e3b5d258584071eb3dcc
Arju <152031111+thearju@users.noreply.github.com>
2024-07-13 02:03:53 +0530

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -5,7 +5,7 @@ export async function before(m, { conn }) {
     let user = global.db.data.users[m.sender]
     if (!user.autolevelup)
         return !0
-    let before = user.level * 20
+    let before = user.level * 1
     while (canLevelUp(user.level, user.exp, global.multiplier))
         user.level++
     user.role = global.rpg.role(user.level).name
